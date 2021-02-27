1ec01afbc9d4be7774bec5d1dbd56f9c1f881ba1
SNEHIL <snehil.e501@example.com>
2021-02-27 21:12:50 +0530

    testing intro to sketch

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function preload(){
 
 function setup(){
   createCanvas(windowWidth, windowHeight, WEBGL);  
-
+  background(0);
   cols = w / scl;
   rows = h / scl;
   for(var x = 0; x < cols; x++){
@@ -25,50 +25,53 @@ function setup(){
 }
 
 function draw(){
-  let time = millis();
-  if(time <= 2000){
-    fill(0);
-    textSize(30);
-    textFont(font);
+
+  let time = frameCount;
+  textAlign(CENTER);
+  fill(145, 206, 235);
+  textFont(font);
+  textSize(30);
+  if(time <= 480){
+  	background(0);
     text('Hi, XYZ', 0, -100);
-  }else if(time <= 4000){
-    fill(0);
-    textSize(30);
-    textFont(font);
-    text('This is a gift for you', 0, 30);
-  }else if(time <= 6000){
-   fill(0);
-   textSize(30);
-   textFont(font);
-   text('from', 0, 60);
-   text('abc', 0, 90);
+	  if(time > 120 && time <= 360){
+	    text('This is a gift for you', 0, -30);
+	    if(time > 240 && time <= 360){
+	      text('from', 0, 50);
+	      text('abc', 0, 80);
+	    }
+	  }else if(time > 360 && time <= 480){
+	    textSize(50);
+	    background(0);
+	    text('3D Terrain', 0, 0);
+	  }
   }else{
     
-    flying -= 0.1;
+    flying -= 0.08;
     var yoff = flying;
     for(var y = 0; y < rows; y++){
       var xoff = 0;
       for(var x = 0; x < cols; x++){
         terrain[x][y] = 
-          map(noise(xoff, yoff), 0, 1, -120, 120);
+          map(noise(xoff, yoff), 0, 1, -100, 100);
         xoff += 0.1;
       }
       yoff += 0.1;
     }
     
-    background(255);
+    background(0);
     stroke(10);
-    fill(50, 50, 200, 180);
+    fill(145, 206, 235, 200);
     
     translate(0, 50);
-    rotateX(PI/2.5);
-    translate(-w/2, -h/2);    
-
+    rotateX(PI/2.6);
+    translate(-w/2, -h/2);
+    
+    stroke(20)
     for(var y = 0; y < rows-1; y++){
       beginShape(TRIANGLE_STRIP);
       for(var x = 0; x < cols; x++){
 
-        //rect(x*scl, y*scl, scl, scl);
         vertex(x*scl, y*scl, terrain[x][y]);
         vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
         
@@ -78,10 +81,10 @@ function draw(){
   }
 }
 
-// function mousePressed() {
-//   if (mouseX > 0 && mouseX < width
-//    && mouseY > 0 && mouseY < height) {
-//     let fs = fullscreen();
-//     fullscreen(!fs);
-//   }
-// }
+function mousePressed() {
+  if(isLooping()){
+  	noLoop();
+  }else{
+  	loop();
+  }
+}
